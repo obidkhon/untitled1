5c8470f1215b0c3ce1f850c2187ec56c9dce965d
Obid Sharipov <obid.sharipov.80@mail.ru>
2021-05-23 14:36:49 +0500

    relf

---
+++ src/RoffelCopter.java
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+public class RoffelCopter {
+
+
+    public static void main(String[] args) throws InterruptedException {
+        while (true){
+
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+
+            System.out.println("   :Lol: ROFL: ROFL: ROFL:                     ");
+            System.out.println("         A             ");
+            System.out.println("    /------                     ");
+            System.out.println("                       ");
+            System.out.println("lol=====       []\\              ");
+            System.out.println(" L  //            ]                ");
+            System.out.println("    //______________                 ");
+            System.out.println("       l            l   ");
+            System.out.println("    -----------------\\");
+            System.out.println("   obid yasagan                   ");
+
+            Thread.sleep(100);
+
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+            System.out.println();
+
+
+
+
+
+
+            System.out.println(" ROFL: ROFL: Lol:                     ");
+            System.out.println("         A             ");
+            System.out.println("  L  /------                     ");
+            System.out.println("                       ");
+            System.out.println("lol=====       []\\              ");
+            System.out.println("    //            ]                ");
+            System.out.println("    //______________                 ");
+            System.out.println("       l            l   ");
+            System.out.println("    -----------------\\");
+            System.out.println("   obid yasagan                   ");
+
+
+            Thread.sleep(100);
+
+        }
+    }
+
+}
